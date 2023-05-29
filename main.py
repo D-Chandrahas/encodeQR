f27bf2f924edc240a2dd6d79bb53c589f8d77957
d-chandrahas <chandrahasreddy732@gmail.com>
2023-05-29 21:34:33 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,50 @@
+# --------------------------------------------------
+# ! DOSEN'T WORK WITH PYPY, ONLY WORKS WITH CPYTHON
+# due to some issues with printing unicode chars
+# --------------------------------------------------
+
+
+import qrcode
+import numpy as np
+import sys
+
+
+# generate qr code as binary matrix
+qr = qrcode.QRCode(
+    version=None,
+    error_correction=qrcode.constants.ERROR_CORRECT_M,
+    box_size=1,
+    border=4,
+)
+qr.add_data(sys.argv[1])
+qr.make(fit=True)
+qr = qr.make_image(fill_color="black", back_color="white")
+qr = np.array(qr)
+
+
+# if height of qr code is odd, add one row of zeros
+if (qr.shape[0]%2 == 1):
+    temp = np.zeros((qr.shape[0]+1, qr.shape[1]), dtype=bool)
+    temp[:qr.shape[0], :] = qr
+    qr = temp
+
+
+# print the qr code using block unicode chars ('▄','█','▀',' ')
+for i in range(0, qr.shape[0], 2):
+
+    for j in range(qr.shape[1]):
+
+        if qr[i,j] == True and qr[i+1,j] == True:
+            print("█", end="")
+
+        elif qr[i,j] == True and qr[i+1,j] == False:
+            print("▀", end="")
+
+        elif qr[i,j] == False and qr[i+1,j] == True:
+            print("▄", end="")
+
+        else:
+            print(" ", end="")
+
+    print("\n", end="")
+
